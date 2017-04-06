449f4c4a0439983dcab2f2986ab2df7e092a410e
Young Wu <doomsplayer@gmail.com>
2017-04-06 12:10:22 +0800

    apex

--- functions/nextaction/src/bin/nextaction.rs
+++ functions/nextaction/src/bin/nextaction.rs
@@ -1,53 +1,35 @@
-#![feature(loop_break_value)]
-
 extern crate nextaction;
 #[macro_use]
 extern crate log;
 extern crate env_logger;
 extern crate config;
-#[macro_use]
-extern crate error_chain;
+extern crate rust_apex;
+extern crate serde_json;
 
-use nextaction::{NextAction, Result};
-use std::thread;
-use std::time::Duration;
-
+use nextaction::{NextAction, Error};
 use config::{Config, File, FileFormat, Environment};
+use serde_json::Value;
 
-quick_main!(run);
-
-fn run() -> Result<()> {
-    let mut c = Config::new();
-
-    c.merge(File::new("Config", FileFormat::Toml).required(false)).unwrap();
-
-    c.merge(Environment::new("NXTT")).unwrap();
-
+fn main() {
     ::env_logger::init().unwrap();
+    rust_apex::run::<_, _, Error, _>(|_: Value, _: rust_apex::Context| {
+        let mut c = Config::new();
+        c.merge(File::new("Config", FileFormat::Toml).required(false)).unwrap();
+        c.merge(Environment::new("NXTT")).unwrap();
 
+        let token = c.get_str("TOKEN").expect("You need to set the NXTT_TOKEN");
 
+        let interval = c.get_int("INTERVAL").unwrap_or(10) as u64;
 
+        let mut na = NextAction::new(&token);
 
-    let token = c.get_str("TOKEN").expect("You need to set the NXTT_TOKEN");
+        c.get("NEXTACTION_NAME").map(|n| na.nextaction_name = n.into_str().unwrap());
+        c.get("SOMEDAY_NAME").map(|n| na.someday_name = n.into_str().unwrap());
 
-    let interval = c.get_int("INTERVAL").unwrap_or(10) as u64;
-
-    let mut na = NextAction::new(&token);
-
-    c.get("NEXTACTION_NAME").map(|n| na.nextaction_name = n.into_str().unwrap());
-    c.get("SOMEDAY_NAME").map(|n| na.someday_name = n.into_str().unwrap());
-
-    let mut result = na.loopit(interval);
-    loop {
-        if let Err(err) = result {
-            warn!("Network issue '{:?}', continuing the loop", err);
-            result = na.loopit(interval);
-        } else {
-            error!("Unexpected error: '{:?}', exiting...", result);
-            thread::sleep(Duration::new(1, 0));
-            break result;
+        if let Err(err) = na.loopit(interval) {
+            error!("Unexpected error: '{:?}', exiting...", err);
         };
-    }?;
-    unreachable!("unreachable!")
+        Ok(())
+    });
 
 }
